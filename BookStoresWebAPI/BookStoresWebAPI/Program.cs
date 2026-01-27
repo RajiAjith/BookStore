b2ed74c8ffb520fed486984db06d49a656358132
Raji Ajith <raji201033@gmail.com>
2026-01-27 20:39:04 +0530

    bookstore models and apis added

---
+++ BookStoresWebAPI/BookStoresWebAPI/Program.cs
@@ -0,0 +1,38 @@
+﻿using BookStoresWebAPI.Controllers;
+using BookStoresWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Use the connection string name that exists in appsettings.json ("BookStoresDB")
+builder.Services.AddDbContext<BookStoresDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookStoresDB")));
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false)
+    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
