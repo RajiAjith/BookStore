32846b3cb9bc62b60a50c0e4f19ae24773f19001
Raji Ajith <raji201033@gmail.com>
2026-01-28 11:13:00 +0530

    Basic Authentication added and some files ignored from git

---
+++ BookStoresWebAPI/BookStoresWebAPI/Handlers/BasicAuthentication.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+
+namespace BookStoresWebAPI.Handlers
+{
+    public class BasicAuthentication : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthentication(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder) : base(options, logger, encoder)
+        {
+
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            return AuthenticateResult.Fail("Not implemented");
+        }
+    }
+}
--- BookStoresWebAPI/BookStoresWebAPI/Program.cs
+++ BookStoresWebAPI/BookStoresWebAPI/Program.cs
@@ -1,8 +1,11 @@
 ﻿using BookStoresWebAPI.Controllers;
+using BookStoresWebAPI.Handlers;
 using BookStoresWebAPI.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +15,14 @@ builder.Services.AddControllers();
 // Use the connection string name that exists in appsettings.json ("BookStoresDB")
 builder.Services.AddDbContext<BookStoresDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("BookStoresDB")));
+
+//ingnore loop handling for use include and theninclude on get data from database
 builder.Services.AddMvc(option => option.EnableEndpointRouting = false)
-    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+//Authentication set up
+builder.Services.AddAuthentication("BasicAuthentication")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthentication>("BasicAuthentication", null);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -29,8 +38,8 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-
-
+//use authentication for users
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
